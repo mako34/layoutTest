865c826d3b423e500f954ecac834e54292710ae2
JuanManuel B <BETA1JUA@police.nsw.gov.au>
2017-07-06 15:32:13 +1000

    prima nocte

---
+++ app/src/main/java/com/prima/n1120697/layouttest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.prima.n1120697.layouttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+    }
+}
